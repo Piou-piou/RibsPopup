82779e89da283d9398d1912ee15b3413b968e16c
Piou-piou <pilloud.anthony@gmail.com>
2018-02-16 13:11:58 +0100

    [func] change package and babel-polifyll

--- webpack.config.js
+++ webpack.config.js
@@ -3,39 +3,37 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const extractSass = new ExtractTextPlugin({
-    filename: "./dist/css/style.css",
-    disable: process.env.NODE_ENV === "development"
+  filename: "./dist/css/style.css",
+  disable: process.env.NODE_ENV === "development"
 });
 
 module.exports = {
-    entry: ['./source/scss/style.scss', './source/js/ribs-popup.js'],
-    output: {
-        filename: 'dist/js/ribs-popup.js'
-        
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                include: path.join(__dirname, 'assets/'),
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: extractSass.extract({
-                    use: [
-                        {loader: 'css-loader'},
-                        {loader: 'sass-loader'}
-                    ],
-                    fallback: "style-loader"
-                })
-            }
-        ]
-    },
-    plugins: [
-        new UglifyJsPlugin(),
-        extractSass
+  entry: ['./source/scss/style.scss', './source/js/ribs-popup.js'],
+  output: {
+    filename: 'dist/js/ribs-popup.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.join(__dirname, 'source/'), path.join(__dirname, 'node_modules/ribs-core/')],
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: extractSass.extract({
+          use: [
+            {loader: 'css-loader'},
+            {loader: 'sass-loader'}
+          ],
+          fallback: "style-loader"
+        })
+      }
     ]
+  },
+  plugins: [
+    new UglifyJsPlugin(),
+    extractSass
+  ]
 };
