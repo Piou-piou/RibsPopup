c31005192923acc4cbb8cece625ed57ca6e37605
Anthony Pilloud <pilloud.anthony@gmail.com>
2018-02-17 21:24:34 +0100

    [func] add method to close a popup

--- source/js/ribs-popup.js
+++ source/js/ribs-popup.js
@@ -9,9 +9,7 @@ class RibsPopup {
     const links = document.querySelectorAll('[data-ribspopup]');
 
     Array.from(links).forEach((element) => {
-      element.addEventListener('click', (event) => {
-        this.openPopup(event);
-      });
+      element.addEventListener('click', (event) => this.openPopup(event));
     })
   }
 
@@ -22,9 +20,59 @@ class RibsPopup {
   openPopup(event) {
     event.preventDefault();
 
-    const popup = event.currentTarget.dataset.popup;
+    const dataPopup = event.currentTarget.dataset.popup;
+    const popup = document.getElementById(dataPopup);
 
-    document.getElementById(popup).classList.add('displayed');
+    popup.classList.add('displayed');
+
+    popup.querySelector('.link .cancel').addEventListener('click', (event) => this.closePopup(event));
+  }
+
+  /**
+   * @param event
+   * method to close a popup
+   */
+  closePopup(event) {
+    event.preventDefault();
+
+    const popup = this.parents(event.currentTarget, '.popup');
+
+    if (popup !== null) {
+      popup.classList.remove('displayed');
+    }
+  }
+
+  /**
+   * @param element
+   * @param wanted
+   * @returns {*}
+   * method to get a wanted parent in parentsNodes of an element
+   */
+  parents(element, wanted) {
+    for ( ; element && element !== document; element = element.parentNode) {
+      if (this.checkWanted(wanted) === 'class' && element.classList.contains(wanted.split('.')[1])) {
+        return element;
+      } else if (this.checkWanted(wanted) === 'id' && element.id === wanted.split('#')[1]) {
+        return element;
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   * @param wanted
+   * @returns {*}
+   * permet de tester si un élément cherché est une class ou un id
+   */
+  checkWanted(wanted) {
+    if (wanted.indexOf('.') !== -1) {
+      return 'class';
+    } else if (wanted.indexOf('#') !== -1) {
+      return 'id';
+    }
+
+    return null;
   }
 }
 
