378f7f12bf72dc124de6efa0dd9bd9169e02089a
Anthony Pilloud <pilloud.anthony@gmail.com>
2018-02-18 16:29:12 +0100

    [func] prepare action on link or submit a form when validate popup

--- source/js/ribs-popup.js
+++ source/js/ribs-popup.js
@@ -20,12 +20,26 @@ class RibsPopup {
   openPopup(event) {
     event.preventDefault();
 
+    const link = event.currentTarget;
     const dataPopup = event.currentTarget.dataset.popup;
     const popup = document.getElementById(dataPopup);
 
     popup.classList.add('displayed');
 
     popup.querySelector('.link .cancel').addEventListener('click', (event) => this.closePopup(event));
+    popup.querySelector('.link .validate').addEventListener('click', (event) => this.setActionValidate(event, link));
+  }
+
+  setActionValidate(event, link) {
+    /*event.preventDefault();*/
+
+    const popup = RibsCore.parents(event.currentTarget, '.popup');
+
+    if (link.dataset.href !== null && link.dataset.href !== undefined) {
+      event.currentTarget.href = link.dataset.href;
+    } else if (link.dataset.form !== null && link.dataset.form !== undefined) {
+      /*document.getElementById(link.dataset.form).*/
+    }
   }
 
   /**
